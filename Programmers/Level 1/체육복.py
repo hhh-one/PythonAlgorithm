9d74ce0db00abbcb812739a96052736c676b1300
hhh-one <904lhw@naver.com>
2022-08-27 23:22:36 +0900

    쳉ㄱ 체육복

---
+++ Programmers/Level 1/체육복.py
@@ -0,0 +1,17 @@
+def solution(n, lost, reserve):
+    # 체육복(uniform) 수를 나타내는 학생 수, 앞 뒤로 한명씩 추가
+    u = [1] * (n + 2) 
+    
+    for i in lost:
+        u[i] -= 1
+    for i in reserve:
+        u[i] += 1
+     
+    # 체육복이 없는 학생 기준으로 앞 -> 뒤 로 빌림
+    for i in range(1, n + 1):
+        if u[i] == 0 and u[i - 1] == 2:
+            u[i - 1:i + 1] = [1, 1]
+        elif u[i] == 0 and u[i + 1] == 2:
+            u[i:i + 2] = [1, 1]
+            
+    return len([x for x in u[1:-1] if x > 0])
