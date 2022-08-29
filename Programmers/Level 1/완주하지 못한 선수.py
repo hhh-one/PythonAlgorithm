d451809fc95161eb60aa25c2d1aee38940150bf2
hhh-one <904lhw@naver.com>
2022-08-29 22:23:49 +0900

    완주하지 못한 선수

---
+++ Programmers/Level 1/완주하지 못한 선수.py
@@ -0,0 +1,9 @@
+def solution(participant, completion):
+    d = {}
+    for x in participant:
+        d[x] = d.get(x, 0) + 1
+    for x in completion:
+        d[x] -= 1
+
+    answer = [k for k,v in d.items() if v == 1]
+    return answer[0]
