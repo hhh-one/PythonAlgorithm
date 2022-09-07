6f674a9b8fc4fefd5eadce2be5209f80ad11c6ca
hhh-one <904lhw@naver.com>
2022-09-07 23:34:22 +0900

    구명보트

---
+++ Programmers/Level 2/구명보트.py
@@ -0,0 +1,12 @@
+def solution(people, limit):
+    answer = len(people)
+    people.sort()
+    l = 0
+    r = len(people) - 1
+
+    while l < r:
+        if people[l] + people[r] <= limit:
+            l += 1
+            answer -= 1
+        r -= 1
+    return answer
